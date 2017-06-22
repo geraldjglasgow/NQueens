436fc5c94cf1153a0ebc3ac3d7938b612d8fb655
Gerald Glasgow <geraldjglasgow@gmail.com>
2017-06-22 12:31:07 -0400

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+/**
+ * Created by SSLGhost on 6/22/17.
+ *
+ */
+
+public class Main {
+    public static void main(String[] args){
+
+    }
+}
---
+++ src/NQueens.java
@@ -0,0 +1,18 @@
+/**
+ * Created by SSLGhost on 6/22/17.
+ *
+ */
+
+public class NQueens {
+
+    private int queens;
+
+    private NQueens(int queens){
+        this.queens = queens;
+    }
+    private void start(){
+        int[][] board = new int[queens][queens];
+
+    }
+
+}
